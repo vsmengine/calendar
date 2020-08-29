64cebfbde68a4ddb479340539ab51c9a3141cd82
V.S.M <sampath.vithanage1@gmail.com>
2020-08-29 21:56:08 +0300

    generate dates according to the month

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calendar';
+  days: number[][] = [];
+  weekDays: string[] = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  months: string[] = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+
+  now = new Date();
+  nowYear = this.now.getFullYear();
+  nowMonth = this.now.getMonth();
+
+  selectedMonthNo: number;
+  selectedMonthName: string;
+
+  constructor() {
+    this.defaultLoad();
+  }
+
+  defaultLoad() {
+    this.selectedMonthNo = this.nowMonth
+    this.selectedMonthName = this.months[this.selectedMonthNo];
+    this.getDaysInMonth(this.nowYear, this.nowMonth);
+  }
+
+  onSelectMonth(selectedMode: string) { 
+    selectedMode == 'next' ? this.selectedMonthNo += 1 : this.selectedMonthNo -= 1;
+    this.selectedMonthName = this.months[this.selectedMonthNo];
+    this.getDaysInMonth(this.nowYear, this.selectedMonthNo);
+  }
+
+  getDaysInMonth(year: number, month: number) {
+    let newDate = new Date(year, month, 1);
+    for (let week = 0; week < 5; week++) {
+      this.days[week] = [];
+      for (let day = newDate.getDay(); day < 7; day++) {
+        if(newDate.getMonth() === month) {
+          this.days[week][day] = newDate.getDate();
+          newDate.setDate(newDate.getDate() + 1);
+        }
+      };
+    };
+  }
+
+
+
 }
