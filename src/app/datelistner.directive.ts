94f354cdb8547698231ec14aa858abcfad409b19
V.S.M <sampath.vithanage1@gmail.com>
2020-09-03 20:40:06 +0300

    create new date seletion

---
+++ src/app/datelistner.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, HostListener, HostBinding, ElementRef, Renderer2 } from '@angular/core';
+import { CalendarService } from './calendar.service';
+
+@Directive({
+  selector: '[appDatelistner]'
+})
+export class DatelistnerDirective {
+
+  constructor(
+    private calendarService: CalendarService,
+    private renderer: Renderer2,
+    private elRef: ElementRef
+  ) { }
+
+  @HostListener('mousedown', ['$event']) onMousedown(event: MouseEvent) {
+    this.calendarService.getStartNode(+event.target['innerHTML']);
+  }
+
+  @HostListener('mouseup', ['$event']) onMouseup(event: MouseEvent) {
+    // this.calendarService.onSelectDates(event.target['innerHTML']);
+    this.calendarService.getEndNode(+event.target['innerHTML']);
+  }
+
+
+}
